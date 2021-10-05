2eb9c58aae96c7eb478ba1580f0d9bc8a8ed21e4
Sean Sanneman <seandafydd@gmail.com>
2021-10-04 18:09:05 -0700

    added banner, testing gradient background

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { Navbar, Nav, Container, Modal, Tab, Button, Row } from 'react-bootstrap';
+import '../../index.css';
+import './Banner.css';
+
+const Banner = () => {
+
+    return (
+        
+        <div className='logo-image  justify-content-center '>
+            <h1>
+                Repo Browser
+                </h1>
+            
+            </div>
+            
+        
+    )
+
+};
+
+export default Banner;
